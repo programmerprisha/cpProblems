ff65651ef317b6823e59d11ce57f797739919cac
programmerprisha <ps.singh.prisha@gmail.com>
2025-05-28 20:14:45 -0500

    Create ContainsDuplicate.java

---
+++ Leetcode/Easy/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+
+       Set<Integer> numbers = new HashSet<>(); 
+
+        for(int i =0; i<nums.length; i++){
+            if(numbers.contains(nums[i]))
+                return true; 
+            else
+                numbers.add(nums[i]);
+        }
+        
+        return false; 
+    }
+}
