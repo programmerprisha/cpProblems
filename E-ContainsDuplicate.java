82f2f876d8f0098e06091772ed998986548eb1ac
programmerprisha <ps.singh.prisha@gmail.com>
2025-05-28 20:17:44 -0500

    Create E-ContainsDuplicate.java

---
+++ E-ContainsDuplicate.java
@@ -0,0 +1,13 @@
+class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+       Set<Integer> numbers = new HashSet<>(); 
+      
+        for(int i =0; i<nums.length; i++){
+            if(numbers.contains(nums[i]))
+                return true; 
+            else
+                numbers.add(nums[i]);
+        }
+        return false; 
+    }
+}
